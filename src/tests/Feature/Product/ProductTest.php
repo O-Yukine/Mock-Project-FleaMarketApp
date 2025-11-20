7f3065b408c5818f24cd77a47fd326d9c2828b00
Yukine <yukineriko@gmail.com>
2025-11-21 05:37:29 +0900

    商品一覧ページのテストコード修正

--- src/tests/Feature/Product/ProductTest.php
+++ src/tests/Feature/Product/ProductTest.php
@@ -15,57 +15,55 @@ class ProductTest extends TestCase
 
     use RefreshDatabase;
 
-    // public function test_anyone_can_access_to_productspage()
-    // {
-    //     $response = $this->get('/');
+    public function test_anyone_can_access_to_productspage()
+    {
+        $response = $this->get('/');
 
-    //     $response->assertStatus(200);
-    // }
+        $response->assertStatus(200);
+    }
 
-    // public function test_user_can_see_all_the_products()
-    // {
-    //     $products = Product::factory()->count(5)->create();
-    //     $categories = Category::factory()->count(3)->create();
+    public function test_unloginUser_can_see_all_the_products()
+    {
+        $products = Product::factory()->count(5)->create();
+        $categories = Category::factory()->count(3)->create();
 
-    //     foreach ($products as $product) {
-    //         $product->categories()->attach($categories->pluck('id')->toArray());
-    //     }
+        foreach ($products as $product) {
+            $product->categories()->attach($categories->pluck('id')->toArray());
+        }
 
-    //     $response = $this->get('/');
+        $response = $this->get('/');
 
-    //     foreach ($products as $product) {
-    //         $response->assertSee($product->name);
-    //     }
-    // }
+        foreach ($products as $product) {
+            $response->assertSee($product->name);
+        }
+    }
 
-    // public function test_only_solditem_have_soldtag()
-    // {
-    //     $category = Category::factory()->create();
+    public function test_only_solditem_have_soldtag()
+    {
+        $category = Category::factory()->create();
 
-    //     $sold = Product::factory()->create();
-    //     $sold->categories()->attach($category->id);
+        $sold = Product::factory()->create();
+        $sold->categories()->attach($category->id);
 
-    //     $notSold = Product::factory()->create();
-    //     $notSold->categories()->attach($category->id);
+        $notSold = Product::factory()->create();
+        $notSold->categories()->attach($category->id);
 
-    //     Purchase::create([
-    //         'id' => 1,
-    //         'user_id' => $sold->user->id,
-    //         'product_id' => $sold->id,
-    //         'payment_method' => 'card',
-    //         'post_code' => '123-4567',
-    //         'address' => '東京都新宿区',
-    //         'building' => null
-    //     ]);
+        Purchase::create([
+            'id' => 1,
+            'user_id' => $sold->user->id,
+            'product_id' => $sold->id,
+            'payment_method' => 'card',
+            'post_code' => '123-4567',
+            'address' => '東京都新宿区',
+            'building' => null
+        ]);
 
+        $response = $this->get('/');
 
-    //     $response = $this->get('/');
+        $response->assertSee('sold');
+    }
 
-    //     $response->assertSee('sold');
-
-    // }
-
-    public function test_sellingItem_inNot_showing()
+    public function test_mySellingItem_inNot_onTheList()
     {
 
         $user1 = User::factory()->create();
