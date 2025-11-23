88ac29b1ac163bce63328cceb83bfbe08355041f
Yukine <yukineriko@gmail.com>
2025-11-23 18:26:38 +0900

    メール認証のテストコード

--- src/tests/Feature/Auth/RegisterTest.php
+++ src/tests/Feature/Auth/RegisterTest.php
@@ -2,11 +2,7 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Support\Facades\Hash;
-use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Support\Facades\Auth;
 use Tests\TestCase;
 
 class RegisterTest extends TestCase
---
+++ src/tests/Feature/Auth/VerificationTest.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Support\Facades\Notification;
+use App\Notifications\CustomVerifyEmail;
+use App\Models\User;
+
+
+class verificationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_user_receives_verification_email()
+    {
+        Notification::fake();
+
+        $this->post('/register', [
+            'name' => 'テストユーザー',
+            'email' => 'test@example.com',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ]);
+
+        $user = User::where('email', 'test@example.com')->first();
+
+        Notification::assertSentTo($user, CustomVerifyEmail::class);
+    }
+
+    public function test_user_can_verify_email_via_link()
+    {
+        Notification::fake();
+
+        $this->post('/register', [
+            'name' => 'テストユーザー',
+            'email' => 'test@example.com',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ]);
+
+        $user = User::where('email', 'test@example.com')->first();
+
+        Notification::assertSentTo($user, CustomVerifyEmail::class, function ($notification, $channels) use ($user, &$verificationUrl) {
+
+            $verificationUrl = $notification->toMail($user)->actionUrl;
+            return true;
+        });
+
+        $this->assertNull($user->email_verified_at);
+
+
+        $response = $this->actingAs($user)->get($verificationUrl);
+
+        $response->assertRedirect('/mypage/profile');
+
+        $user->refresh();
+        $this->assertNotNull($user->email_verified_at);
+    }
+
+    public function test_it_redirects_to_profile_after_verify()
+    {
+        Notification::fake();
+
+        $this->post('/register', [
+            'name' => 'テストユーザー',
+            'email' => 'test@example.com',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ]);
+
+        $user = User::where('email', 'test@example.com')->first();
+
+        Notification::assertSentTo($user, CustomVerifyEmail::class, function ($notification, $channels) use ($user, &$verificationUrl) {
+
+            $verificationUrl = $notification->toMail($user)->actionUrl;
+            return true;
+        });
+
+        $this->assertNull($user->email_verified_at);
+
+        $response = $this->actingAs($user)->get($verificationUrl);
+
+        $response->assertRedirect('/mypage/profile');
+    }
+}
